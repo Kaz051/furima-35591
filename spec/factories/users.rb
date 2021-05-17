c3d580294bad5b50c0e561707dc22b2b047d9df4
Kaz051 <kznlion@gmail.com>
2021-05-18 04:52:28 +0900

    Userモデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"test"}
+    email                 {Faker::Internet.free_email}
+    password              {"000aaa"}
+    password_confirmation {"000aaa"}
+    last_name             {"中村"}
+    first_name            {"豊"}
+    last_name_kana        {"ナカムラ"}
+    first_name_kana       {"ユタカ"}
+    birthday              {"2020/10/01"}
+  end
+end
