deccc6f99e33728028d12c7b618dbcf114ff6109
Kaz051 <kznlion@gmail.com>
2021-05-18 04:54:52 +0900

    Rubocopで自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birthday, presence: true
- 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
-  
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name
     validates :last_name
@@ -19,5 +19,4 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-
 end
