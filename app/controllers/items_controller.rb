83bfa3f9884706778bd9070dcb10abd29bfa06a3
Kaz051 <kznlion@gmail.com>
2021-05-19 00:14:22 +0900

    ActiveStorageの導入2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:user, :name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id )
+    params.require(:item).permit(:user, :name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :image )
   end
 end
