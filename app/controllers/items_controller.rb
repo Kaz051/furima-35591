17c0d6fd633a27883aefaa764450e9f865e94163
Kaz051 <kznlion@gmail.com>
2021-05-18 23:28:12 +0900

    商品出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user, :name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id )
   end
 end
