74b93ec1481a54a9652fa0c92feb4e6a98b6025a
Kaz051 <kznlion@gmail.com>
2021-05-18 23:26:11 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :user
+    validates :name
+    validates :price
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
 end
