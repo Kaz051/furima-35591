3d0a06c8f76496c7c18a4d455d2483758e914478
Kaz051 <kznlion@gmail.com>
2021-05-19 16:14:52 +0900

    categoryのActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  belongs_to :category
 
   belongs_to :user
   has_one_attached :image
 
-  validates :prefecture_id, numericality: { other_than: 1 } 
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+    validates :category_id
+  end   
 
   with_options presence: true do
     validates :user
@@ -18,5 +22,4 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :shipping_date_id
   end
-
 end
