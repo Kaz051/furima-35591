ec0fc99a5916bc52d067e762c917c693b2450e8e
Kaz051 <kznlion@gmail.com>
2021-05-19 15:33:57 +0900

    prefectureのActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   belongs_to :user
   has_one_attached :image
+
+  validates :prefecture_id, numericality: { other_than: 1 } 
 
   with_options presence: true do
     validates :user
