a459cf7a115f46e5a1ea9b6ba68e043b47842367
Kaz051 <kznlion@gmail.com>
2021-05-20 12:46:03 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,9 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_cost
   belongs_to :shipping_date
+  has_one_attached :image
 
   belongs_to :user
-  has_one_attached :image
 
   with_options numericality: { other_than: 1 } do
     validates :prefecture_id
@@ -27,5 +27,6 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
+    validates :image
   end
 end
