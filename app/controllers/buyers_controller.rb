babbf262d4b952d7e3b32b1c776e19ecc624fbab
Kaz051 <kznlion@gmail.com>
2021-05-23 10:22:04 +0900

    Formオブジェクトのインスタンスを生成

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,26 @@
 class BuyersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @buyer_destination = BuyerDestination.new
+  end
+
+  def create
+    @buyer_destination = BuyerDestination.new(buyer_params)
+    if @buyer_destination.valid?
+      @buyer_destination.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
 end
