656465b4936c3fe463026bf5ee4d826b7e32820d
Kaz051 <kznlion@gmail.com>
2021-05-24 17:06:51 +0900

    Formオブジェクトの実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,10 +1,11 @@
 class BuyersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    @buyer_destination = BuyerDestination.new
   end
 
   def new
-    @buyer_destination = BuyerDestination.new
   end
 
   def create
@@ -13,14 +14,14 @@ class BuyersController < ApplicationController
       @buyer_destination.save
       redirect_to root_path
     else
-      render :new
+       render :new
     end
   end
 
   private
 
   def buyer_params
-    params.require(:buyer_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
+    params.require(:buyer_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 
 end
--- app/models/buyer_destination.rb
+++ app/models/buyer_destination.rb
@@ -1,11 +1,10 @@
 class BuyerDestination
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :buyer_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :buyer_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id
     validates :city
@@ -18,6 +17,6 @@ class BuyerDestination
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
     Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, buyer_id: buyer.id)
-  endbuyer
+  end
 
 end
