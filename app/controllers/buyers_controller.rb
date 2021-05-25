443864fc5fc29516881ae8735e1b865309138bc0
Kaz051 <kznlion@gmail.com>
2021-05-26 05:41:31 +0900

    クレジットカード決済機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -13,6 +13,7 @@ class BuyersController < ApplicationController
   def create
     @buyer_destination = BuyerDestination.new(buyer_params)
     if @buyer_destination.valid?
+      pay_item
       @buyer_destination.save
       redirect_to root_path
     else
@@ -23,7 +24,7 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.require(:buyer_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+    params.require(:buyer_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -34,4 +35,12 @@ class BuyersController < ApplicationController
     redirect_to root_path if current_user.id == @item.user_id || @item.buyer.present?
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_5aa229067d17d928be08df42"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
