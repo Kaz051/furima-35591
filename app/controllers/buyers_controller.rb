0cee3a1235756173fc93b59b402c38b48fb849e4
Kaz051 <kznlion@gmail.com>
2021-05-25 09:25:49 +0900

    トップページへ遷移

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,7 +1,9 @@
 class BuyersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: :index
 
   def index
-    @item = Item.find(params[:item_id])
     @buyer_destination = BuyerDestination.new
   end
 
@@ -9,7 +11,6 @@ class BuyersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_destination = BuyerDestination.new(buyer_params)
     if @buyer_destination.valid?
       @buyer_destination.save
@@ -23,6 +24,14 @@ class BuyersController < ApplicationController
 
   def buyer_params
     params.require(:buyer_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id || @item.buyer.present?
   end
 
 end
