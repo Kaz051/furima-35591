228765153e16ce3170bed9143cd7d971b962b8c6
Kaz051 <kznlion@gmail.com>
2021-05-26 12:40:54 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "items#index"
 
   resources :items do
-    resources :buyers, only: [:index, :new, :create]
+    resources :buyers, only: [:index, :create]
   end
 end
